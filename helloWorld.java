ee316ca574cc328f422773fade6f3bf65735fbe8
adhocmahen <39306086+adhocmahen@users.noreply.github.com>
2018-05-20 14:03:09 +0530

    Hello World adding 10 from GUI
    Hello World adding 10 from GUI

--- helloWorld.java
+++ helloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World delete 2");
+		System.out.println("Hello World adding 10 from GUI");
+		
 	}
 }
