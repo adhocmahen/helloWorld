3b8e4f0efad4471036a292f1ab12df3f0e5cfb97
adhocmahen <mahendran.nature@gmail.com>
2018-05-20 14:10:30 +0530

    Revert "Hello World only"
    This reverts commit 79627dd38b509a8995470aa7af65fb563b8a46bc.

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,7 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World only");
+		System.out.println("Hello World delete 2");
+		System.out.println("Hello World add 30 from eclipse");		
 	}
 }
