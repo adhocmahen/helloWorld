eba34a3e81e99130c58674accc5b94e8fc296b0f
adhocmahen <mahendran.nature@gmail.com>
2018-05-20 14:06:15 +0530

    Hello World add 30 from eclipse

--- helloWorld.java
+++ helloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World delete 2");
-		System.out.println("Hello World adding 10 from GUI");
-		
+		System.out.println("Hello World add 30 from eclipse");		
 	}
 }
