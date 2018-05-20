79627dd38b509a8995470aa7af65fb563b8a46bc
adhocmahen <39306086+adhocmahen@users.noreply.github.com>
2018-05-20 14:08:48 +0530

    Hello World only
    Hello World only

--- helloWorld.java
+++ helloWorld.java
@@ -4,7 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World delete 2");
-		System.out.println("Hello World add 30 from eclipse");		
+		System.out.println("Hello World only");
 	}
 }
